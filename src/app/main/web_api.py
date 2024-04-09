96398896e70453e1e6ab2bd91bdc4ebef5ed1fa9
alim <Abrekovalim38702@gmail.com>
2024-04-09 03:44:06 +0300

    feat: autoinject to router

--- src/app/main/web_api.py
+++ src/app/main/web_api.py
@@ -1,12 +1,21 @@
-from dishka.integrations.fastapi import setup_dishka
-from fastapi import FastAPI
+from functools import partial
+
+from dishka.integrations.fastapi import DishkaRoute, setup_dishka
+from fastapi import APIRouter, FastAPI
 
 from app.infrastructure.di.main import container_factory
 from app.presentation.web_api.exc_handlers import init_exception_handlers
 from app.presentation.web_api.routers import health_check_router
 
 
-def init_di(app: FastAPI) -> None:
+def init_di(app: FastAPI, autoinject: bool = True) -> None:
+    def include_router_autoinject(app_: FastAPI, router: APIRouter, **kwargs) -> None:
+        setattr(router, "route_class", DishkaRoute)
+        app_.router.include_router(router, **kwargs)
+
+    if autoinject:
+        setattr(app, "include_router", partial(include_router_autoinject, app))
+
     setup_dishka(container_factory(), app)
 
 
